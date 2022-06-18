1cd5cd3745f28f3b2fb6552da3e1a0154ad14327
zzliu <tyliu0914@gmail.com>
2022-06-19 01:42:16 +0800

    v1.1:splash screen

---
+++ app/src/main/java/pucsim4/caterpillar/splash.kt
@@ -0,0 +1,24 @@
+package pucsim4.caterpillar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.HandlerCompat.postDelayed
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
